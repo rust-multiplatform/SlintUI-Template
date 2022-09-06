2b03818fd7421f5e1094a0f98eee01c492d6ec16
Lukas Weber <me@sakul6499.de>
2022-09-06 21:12:33 +0200

    Disabled all tests

--- platform/ios/src/tests/mod.rs
+++ platform/ios/src/tests/mod.rs
@@ -1,7 +1,7 @@
 use crate::main_rs;
 
-#[test]
-#[cfg(feature = "ui-tests")]
-fn check_main_rs_exists() {
-    main_rs();
-}
+// #[test]
+// #[cfg(feature = "ui-tests")]
+// fn check_main_rs_exists() {
+//     main_rs();
+// }
--- shared/src/tests/mod.rs
+++ shared/src/tests/mod.rs
@@ -1,7 +1,7 @@
 use crate::entrypoint;
 
-#[test]
-#[cfg(feature = "ui-tests")]
-fn check_entrypoint_exists() {
-    entrypoint();
-}
+// #[test]
+// #[cfg(feature = "ui-tests")]
+// fn check_entrypoint_exists() {
+//     entrypoint();
+// }
